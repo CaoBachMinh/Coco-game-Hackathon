db35a4dad662af8034323ac803a7e1f249000b96
BachMinh <bachdangthaiminh@gmail.com>
2023-11-24 20:53:17 +0700

    update user name

--- src/components/Personal/about-personal.jsx
+++ src/components/Personal/about-personal.jsx
@@ -55,7 +55,7 @@ const Personal = ({ title }) => {
                                     backgroundClip: "border-box",
 
                                 }}>
-                                    <h5 className="my-3">John Smith</h5>
+                                    <h5 className="my-3">{displayName}</h5>
                                     <p className="text-muted mb-1">Full Stack Developer</p>
                                     <p className="text-muted mb-4">Bay Area, San Francisco, CA</p>
                                     <div className="d-flex justify-content-center mb-2">
